a4165596be23205e0e125b8869e8865675c5d09d
khoi <le.khoi992@gmail.com>
2015-11-23 17:28:03 +0700

    w

--- sftp.py
+++ sftp.py
@@ -1,14 +1,15 @@
 import json
+import os
 import pysftp
 from os import walk
 
 settings = None
 with open("settings.txt") as data_file:
     settings = json.load(data_file)
-with pysftp.Connection(settings["ip"], username=settings["id"], password=settings["password"]) as sftp:
+with pysftp.Connection(settings["ip_address"], username=settings["username"], password=settings["password"]) as sftp:
     with sftp.cd(settings["target_location"]):
         f = []
-        for (dirpath, dirnames, filenames) in walk(settings["target_location"]):
+        for (dirpath, dirnames, filenames) in walk(settings["location"]):
                 for filename in filenames:
-                    sftp.put(filename)
+                    sftp.put(os.path.abspath(settings["location"] + "/" + filename))
                 break
